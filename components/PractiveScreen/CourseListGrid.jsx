57e8eb928f88dd6763894f33c69be95633ba1875
DucNghia176 <nghi7a@gmail.com>
2025-05-11 22:46:22 +0700

    no

--- components/PractiveScreen/CourseListGrid.jsx
+++ components/PractiveScreen/CourseListGrid.jsx
@@ -1,5 +1,6 @@
 import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
+import Colors from '../../constants/Colors'
 
 export default function CourseListGrid({ courseList, option }) {
     return (
@@ -15,20 +16,26 @@ export default function CourseListGrid({ courseList, option }) {
                         style={{
                             flex: 1,
                             display: 'flex',
-                            flexDirection: 'row',
+                            flexDirection: 'column',
                             alignItems: 'center',
-                            justifyContent: 'center'
+                            justifyContent: 'center',
+                            padding: 15,
+                            backgroundColor: Colors.WHITE,
+                            margin: 7,
+                            borderRadius: 15
                         }}
                     >
-                        <Image source={option?.icon}
+                        <Image source={option.icon}
                             style={{
                                 width: 100,
                                 height: 100,
                                 objectFit: 'contain'
                             }}
                         />
+                        <Text>{item.courseTitle}</Text>
                     </View>
                 )}
+
             />
         </View>
     )
